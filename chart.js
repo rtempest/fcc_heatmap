e29761efe859742b2f3eaccbefaf4fdbd974293c
Rosie Tempest <rosietempest@gmail.com>
2021-01-09 15:37:15 -0800

    fetched the json data

---
+++ chart.js
@@ -0,0 +1,10 @@
+// get the data
+url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
+
+d3.json(url, (error, json) => {
+
+    console.log(json)
+
+
+})
+
