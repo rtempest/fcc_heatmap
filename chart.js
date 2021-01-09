0d5e615071a34b3ba2c8acc06d6c7394078626c3
Rosie Tempest <rosietempest@gmail.com>
2021-01-09 15:47:45 -0800

    set up svg with title and description

--- chart.js
+++ chart.js
@@ -4,7 +4,35 @@ url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 d3.json(url, (error, json) => {
 
     console.log(json)
+    const baseTemp = json.baseTemperature
+    console.log(baseTemp)
 
+    // define dimensions and position
+    const h = 600
+    const w = 800
+    const pX = 50
+    const pY = 50
 
+    // create svg
+    const svg = d3.select('#chart')
+        .append('svg')
+        .attr('height', h)
+        .attr('width', w)
+
+    // create title
+    svg.append('text')
+        .attr('id', 'title')
+        .attr('x', w / 2)
+        .attr('y', pY)
+        .text('Average Land Surface Temperature')
+        .attr('text-anchor', 'middle')
+
+    // create description
+    svg.append('text')
+        .attr('id', 'description')
+        .attr('x', w / 2)
+        .attr('y', pY + 30)
+        .text('description bla bla bla')
+        .attr('text-anchor', 'middle')
 })
 
